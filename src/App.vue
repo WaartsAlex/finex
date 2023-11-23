671d0cc5370914ae017e9e3423b3e90581f1c1aa
Alex <waartsalex@gmail.com>
2023-11-23 21:43:50 +0100

    Added main page

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,29 @@
+<script setup>
+  import ExcerciseHeader from './components/ExcerciseHeader.vue'
+  import FinexHeader from './components/FinexHeader.vue'
+  import FinexNews from './components/FinexNews.vue'
+  import FinexAdmin from './components/FinexAdmin.vue'
+  import { ref } from 'vue'
+
+  const admin = ref(false);
+
+  function handleCodeUpdate(update) {
+    admin.value = update;
+  }
+</script>
+
 <template>
-  <div>
-    <ExcerciseHeader class="m-3"/>
-    <FinexHeader />
-    <FinexNews />
+  <div class="bg-gray">
+    {{admin}}
+    <ExcerciseHeader class="fixed text-center w-full top-0 p-6"/>
+    <FinexHeader @code-update="handleCodeUpdate"/>
+    <FinexAdmin v-if="admin"/>
+    <FinexNews v-else/>
   </div>
 </template>
 
-<script>
-import ExcerciseHeader from './components/ExcerciseHeader.vue'
-import FinexHeader from './components/FinexHeader.vue'
-import FinexNews from './components/FinexNews.vue'
-
-export default {
-  name: 'App',
-  components: {
-    ExcerciseHeader,
-    FinexHeader,
-    FinexNews
-  }
-}
-</script>
-
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
--- src/components/FinexNews.vue
+++ src/components/FinexNews.vue
@@ -8,13 +8,11 @@ const articles = useCollection(collection(db, "Articles"));
 </script>
 
 <template>
-    <main class="flex flex-col items-center">
+    <main class="flex flex-col items-center m-6">
       <FinexArticle 
         v-for="article in articles" :key="article.id"
-        :title="article.Title"
-        :message="article.Message"
-        :author="article.Author"
-        :image="article.Image"
+        :article="article"
+        class="my-12"
       />
     </main>
 </template>
